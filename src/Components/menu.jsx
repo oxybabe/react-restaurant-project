3c7a3d2188b2eaa704a2540e27d464d0674029fd
Elia Partin <arenaselia@gmail.com>
2023-06-03 10:02:11 -0400

    added menu items

---
+++ src/Components/menu.jsx
@@ -0,0 +1,122 @@
+const INITIAL_MENU = [
+  {
+    id: 1,
+    title: "Fried Spring Roll",
+    tag: "appetizer",
+    description:
+      "Ground chicke, tofu, egg, cucumber and bean sprouts rolled in rice paper, served with tamarind sauce",
+    imgID: "",
+    price: 2.0,
+  },
+  {
+    id: 2,
+    title: "Vegetarian Spring Roll",
+    tag: "appetizer",
+    description:
+      "Tofu, egg, cucumber, carrot and bean sprouts rolled in rice paper, served with tamarind sauce",
+    imgID: "",
+    price: 3.0,
+  },
+  {
+    id: 3,
+    title: "Steamed Vegetable Dumpling",
+    tag: "appetizer",
+    description:
+      "Seasonal vegetables and tofu in a soft dumpling, steamed and served with a spicy basil dipping sauce",
+    imgID: "",
+    price: 5.0,
+  },
+  {
+    id: 4,
+    title: "Pad Thai",
+    tag: "entree",
+    description:
+      "Thai rice noodles stir friend with egg, bean sprouts, green onions, ground peanut",
+    imgID: "",
+    price: 9.0,
+  },
+  {
+    id: 5,
+    title: "Red Curry",
+    tag: "entree",
+    description:
+      "Spice red curry with coconut milk, bamboo shoots, bell pepper and basil leaves",
+    imgID: "",
+    price: 8.0,
+  },
+  {
+    id: 6,
+    title: "Pad See Ew",
+    tag: "entree",
+    description:
+      "Thai rice noodles with broccoli, carrot, mushroom,in a sweet soy sauce",
+    imgID: "",
+    price: 9.0,
+  },
+  {
+    id: 7,
+    title: "Thai Lo Mein",
+    tag: "entree",
+    description:
+      "Egg noodles cooked in light tomato sauce with mixed vegetables",
+    imgID: "",
+    price: 9.0,
+  },
+  {
+    id: 8,
+    title: "Yellow Curry",
+    tag: "entree",
+    description:
+      "Mild yellow curry with coconut milk, potatoes, carrots and onions",
+    imgID: "",
+    price: 8.0,
+  },
+  {
+    id: 9,
+    title: "Fountain Soda",
+    tag: "beverage",
+    description: "Coke products",
+    imgID: "",
+    price: 2.0,
+  },
+  {
+    id: 10,
+    title: "Thai Iced Tea",
+    tag: "beverage",
+    description: "with tapioca pearls",
+    imgID: "",
+    price: 3.0,
+  },
+  {
+    id: 11,
+    title: "Thai Coffee",
+    tag: "beverage",
+    description: "hot or cold",
+    imgID: "",
+    price: 2.0,
+  },
+  {
+    id: 12,
+    title: "Sticy Rice with Mango/Custard",
+    tag: "dessert",
+    description: "",
+    imgID: "",
+    price: 6.0,
+  },
+  {
+    id: 13,
+    title: "Coconut Ice Cream",
+    tag: "dessert",
+    description: "",
+    imgID: "",
+    price: 6.0,
+  },
+  {
+    id: 14,
+    title: "Pa Tong Go",
+    tag: "dessert",
+    description: "Thai doughnuts",
+    imgID: "",
+    price: 6.0,
+  },
+];
